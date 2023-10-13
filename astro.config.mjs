24df17f0156a13c21d7827d321e4518ec764f06c
Rahul Deshar <desarrahul@gmail.com>
2023-10-13 20:50:40 +0545

    update title

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rahuldesar.github.io',
+  site: 'https://www.rahuldeshar.com.np',
   base: '/',
+  integrations: [tailwind(), react()]
 });
