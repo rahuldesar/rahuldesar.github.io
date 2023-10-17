211da26375f7340c7efc901077f0011b3bbfe3d6
Rahul Deshar <desarrahul@gmail.com>
2023-10-17 22:11:49 +0545

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.rahuldeshar.com.np',
-  base: '/',
-  integrations: [tailwind(), react()]
+  site: "https://www.rahuldeshar.com.np",
+  base: "/",
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
