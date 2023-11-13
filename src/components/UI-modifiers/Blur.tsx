d5cfbf1aceae1071822a78110cd50f122bef13e3
Rahul Deshar <desarrahul@gmail.com>
2023-11-13 10:37:06 +0545

    added og image

--- src/components/UI-modifiers/Blur.tsx
+++ src/components/UI-modifiers/Blur.tsx
@@ -32,12 +32,16 @@ const Blur = () => {
     <div className="absolute inset-0 overflow-hidden bg-background ">
       <div
         ref={blobRef}
-        className="translate-middle absolute aspect-square h-60 animate-custom-rotate rounded-full bg-gradient-to-r from-green-400  to-red-500 opacity-30 ease-in-out"
+        className="translate-middle absolute aspect-square h-60 animate-custom-rotate rounded-full bg-gradient-to-r from-catDarkGreen  to-catDarkRosewater opacity-30 ease-in-out"
       ></div>
       <div
         id="blur"
         className="absolute z-10 h-full w-full backdrop-blur-[120px]"
       ></div>
+
+      <div className="rounded-[67%] rounded-t-[33%] rounded-r-[100%] rounded-b-[0%] 
+            rounded-tl-[100%] rounded-tr-[26%] rounded-br-[74%] rounded-bl-[0%] h-60 animate-custom-rotate rounded-full bg-gradient-to-r from-green-400  to-red-500 opacity-30 ease-in-out  top-50 left-3/4 z-20 absolute">
+      </div>
     </div>
   );
 };
