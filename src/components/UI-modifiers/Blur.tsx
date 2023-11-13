2d82da11d6aec09dae1ac917fb0497c5adfeee54
Rahul Deshar <desarrahul@gmail.com>
2023-11-13 15:39:57 +0545

    update homepage - added experiences layout

--- src/components/UI-modifiers/Blur.tsx
+++ src/components/UI-modifiers/Blur.tsx
@@ -22,7 +22,7 @@ const Blur = () => {
             left: `${clientX}px`,
             top: `${top}px`,
           },
-          { duration: 2500, fill: "forwards" }
+          { duration: 1000, fill: "forwards" }
         );
       }
     }
@@ -39,9 +39,10 @@ const Blur = () => {
         className="absolute z-10 h-full w-full backdrop-blur-[120px]"
       ></div>
 
-      <div className="rounded-[67%] rounded-t-[33%] rounded-r-[100%] rounded-b-[0%] 
-            rounded-tl-[100%] rounded-tr-[26%] rounded-br-[74%] rounded-bl-[0%] h-60 animate-custom-rotate rounded-full bg-gradient-to-r from-green-400  to-red-500 opacity-30 ease-in-out  top-50 left-3/4 z-20 absolute">
-      </div>
+      <div
+        className="top-50 absolute left-3/4 z-20 
+            h-60 animate-custom-rotate rounded-[67%] rounded-full rounded-b-[0%] rounded-r-[100%] rounded-t-[33%] rounded-bl-[0%] rounded-br-[74%]  rounded-tl-[100%] rounded-tr-[26%] bg-gradient-to-r  from-green-400 to-red-500 opacity-30 ease-in-out"
+      ></div>
     </div>
   );
 };
